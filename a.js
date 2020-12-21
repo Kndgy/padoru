9d6812b3fc79572a3dc66d40b0f81fd58d9fde75
Rei <53145560+Kndgy@users.noreply.github.com>
2020-12-21 07:41:18 +0700

    Update a.js

--- a.js
+++ a.js
@@ -12,8 +12,8 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("demo").innerHTML = days + " Day " + hours + " Hours "
-  + minutes + " Minute " + seconds + " Second ";
+  document.getElementById("demo").innerHTML = days + " Days " + hours + " Hours "
+  + minutes + " Minutes " + seconds + " Seconds ";
 
   if (distance < 0) {
     clearInterval(x);
